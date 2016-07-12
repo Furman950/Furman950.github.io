85a3a143e25d7a71dc4d896a070892339fb3e6bb
Fermin Sandoval <ferminsandoval950@hotmail.com>
2016-07-12 01:44:12 +0000

    adding memes page

---
+++ scripts.js
@@ -0,0 +1,19 @@
+var number, counter = 0, numTracker;
+
+function numGen (){
+    number = Math.floor((Math.random() * 9) + 1);
+}
+
+function checkIfMemeVis (){
+    if (counter > 0){
+    document.getElementById(""+number+"").style.display = "none";
+    }
+    counter++;    
+}
+
+function memeGen () {
+    checkIfMemeVis();
+    numGen();
+    document.getElementById(""+number+"").style.display = "inline";
+
+}
